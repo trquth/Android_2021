bce6b7e3ace3e593b4c02e25022a763a8edee9f4
Thien Tran <thien.tran@dfo.global>
2021-05-09 00:46:52 +0700

    Building interactive app

--- app/src/main/java/com/example/androidforbeginner/MainActivity.java
+++ app/src/main/java/com/example/androidforbeginner/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.androidforbeginner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private  BeerExpert expert = new BeerExpert();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickFindBeer(View view){
+        System.out.println("Do");
+        Spinner color = (Spinner) findViewById(R.id.color);
+        String beerType = String.valueOf(color.getSelectedItem());
+//        System.out.println(beerType);
+      TextView brands = (TextView) findViewById(R.id.brands);
+//        brands.setText(beerType);
+        List<String> brandsList = expert.getBrands(beerType);
+        StringBuilder brandsFormetted = new StringBuilder();
+        for (String brand: brandsList){
+            brandsFormetted.append(brand).append('\n');
+        }
+        brands.setText(brandsFormetted);
+
     }
 }
