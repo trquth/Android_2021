1e8a5ed7329ea0bd64c2bc81bfae532636d045a4
Thien Tran <thien.tran@dfo.global>
2021-05-10 21:26:00 +0700

    Done

---
+++ app/src/main/java/com/example/androidforbeginner/LayoutConstraintActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidforbeginner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LayoutConstraintActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_constraint);
+    }
+}
