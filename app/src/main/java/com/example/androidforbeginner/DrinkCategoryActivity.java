580bd77f5666e38c853719cf6b221da1a75ededc
Thien Tran <thien.tran@dfo.global>
2021-05-11 22:44:37 +0700

    Done

---
+++ app/src/main/java/com/example/androidforbeginner/DrinkCategoryActivity.java
@@ -0,0 +1,35 @@
+package com.example.androidforbeginner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class DrinkCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink_category);
+
+        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,Drink.drinks);
+
+        ListView listView  = (ListView) findViewById(R.id.list_drink);
+        listView.setAdapter(listAdapter);
+
+        //Create listener
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(DrinkCategoryActivity.this, DrinkActivity.class);
+                intent.putExtra(DrinkActivity.EXTRA_DRINDID, (int)id);
+                startActivity(intent);
+            }
+        };
+        listView.setOnItemClickListener(itemClickListener);
+    }
+}
